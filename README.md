81dfc028d8e68d44339bfcbc35d5f6edd859d4a8
rmonterodev <rmontero1990@gmail.com>
2020-04-29 18:49:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-isjf6qrmm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-isjf6qrmm)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS tricky questions</h1>`;
+
+//concantened. + means used for strings when dont have at least two numbers. Converted the other value into a string.
+console.log(2 + '2') 
+
+console.log(2 - '2') 
+
